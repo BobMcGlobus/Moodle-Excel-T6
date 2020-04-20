80625ab144ee140fc4d1f11bc218bf20753a6bae
chrisdancer <c.daenzer@otterbach.de>
2020-04-20 10:27:42 +0200

    added electron project

---
+++ 01_front-End/01_App/src/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  // Erstelle das Browser-Fenster.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // und lade die index.html der App.
+  win.loadFile('index.html')
+
+  // Öffnen der DevTools.
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Einige APIs können nur nach dem Auftreten dieses Events genutzt werden.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // Unter macOS ist es üblich, für Apps und ihre Menu Bar
+  // aktiv zu bleiben, bis der Nutzer explizit mit Cmd + Q die App beendet.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
+  // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
