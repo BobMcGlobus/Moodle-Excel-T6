69c4bbf29bb1178c9a433f4b6ff333cacc294061
chrisdancer <c.daenzer@otterbach.de>
2020-04-23 11:07:01 +0200

    load html-file with __dirname

--- 01_front-End/01_App/src/main.js
+++ 01_front-End/01_App/src/main.js
@@ -21,7 +21,7 @@ function createWindow () {
   // Menu entfernen
   // win.removeMenu();
   Menu.setApplicationMenu(false)
-  win.loadURL('http://127.0.0.1:5500/01_App/src/html/index.html')
+  win.loadURL('file://' + __dirname + '/html/index.html');
 }
 app.whenReady().then(createWindow);
 // This method will be called when Electron has finished
