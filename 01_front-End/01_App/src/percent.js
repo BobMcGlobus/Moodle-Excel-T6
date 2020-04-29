b1c12a4ac6f7e87c192828600c99a35a1f8b0bb1
Jonas <jonas.althoff@outlook.de>
2020-04-29 15:02:06 +0200

    Update percent.js

--- 01_front-End/01_App/src/percent.js
+++ 01_front-End/01_App/src/percent.js
@@ -1,11 +1,11 @@
-var i = 0;
+var m = 0;
 
 var perc = ['0%', '13%', '17%', '25%', '37%', '49%', '51%', '62%', '83%', '91%' ];
 
 function percent() {
-    if (i < 9) {
-        document.getElementById("percent").innerHTML = perc[i];
-        i++;
+    if (m < 9) {
+        document.getElementById("percent").innerHTML = perc[m];
+        m++;
         setTimeout(percent, 2500);
     }else{
         document.getElementById("percent").innerHTML = "100%";
